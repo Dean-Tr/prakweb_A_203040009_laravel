1189c81318a1a648bc9f3b4e6cd37f144db25233
Dean-Tr <deantirtas@gmail.com>
2022-10-17 19:46:12 +0700

    Menyelesaikan video 3 Laravel Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Dean Tirta S",
+        "email" => "203040009@unpas.mail.ac.id",
+        "image" => "foto.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

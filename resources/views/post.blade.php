d7a0b1d32e4f38ed8b0f5653f6e2d0ad74b6fc7c
Dean-Tr <deantirtas@gmail.com>
2022-10-31 22:43:16 +0700

    Menyelesaikan video 13 Laravel Searching & Pagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,11 +8,11 @@
 
                 <p>
                     By.
-                    <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">
+                    <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">
                         {{ $post->author->name }}
                     </a>
                     in
-                    <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+                    <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">
                         {{ $post->category->name }}
                     </a>
                 </p>

65d1fc8eff9feee85a27e20d89f59c22cdd25cb2
Dean-Tr <deantirtas@gmail.com>
2022-11-21 14:51:09 +0700

    Menyelesaikan video 19 Laravel Validation & Insert Post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
@@ -41,7 +42,19 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        $ValidatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
+
+        $ValidatedData['user_id'] = auth()->user()->id;
+        $ValidatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::create($ValidatedData);
+
+        return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
 
     /**

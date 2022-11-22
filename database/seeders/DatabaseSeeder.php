64afaf381c3390ca54cecec47a8b3cffa3b9f752
Dean-Tr <deantirtas@gmail.com>
2022-11-22 21:04:49 +0700

    Menyelesaikan video 21 Laravel Upload Image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,11 +24,12 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // User::create([
-        //     'name' => 'Dean Tirta',
-        //     'email' => 'deantirtas@gmail.com',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Dean Tirta',
+            'username' => 'dean',
+            'email' => 'deantirtas@gmail.com',
+            'password' => bcrypt('password')
+        ]);
 
         // User::create([
         //     'name' => 'Jhdksa',
